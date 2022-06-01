0e400d8179937bf4f8eeb058a28af02fc8320f4e
30Owls <oforolega@gmail.com>
2022-06-01 19:05:09 +0300

    fix(fs): rename.js - done

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,22 @@
+import fs from 'fs';
+
 export const rename = async () => {
-    // Write your code here 
+    let srcFile = './src/fs/files/wrongFilename.txt';
+    let destFile = './src/fs/files/properFilename.md';
+
+    fs.access(srcFile, fs.constants.F_OK, (err) => {
+        if(err){
+            throw new Error('FS operation failed');
+        }
+        fs.access(destFile, fs.constants.F_OK, (err) => {
+            if(!err){
+                throw new Error('FS operation failed');
+            }
+            fs.rename(srcFile, destFile, function(err) {
+                if ( err ) console.log('ERROR: ' + err);
+            });
+        });      
+    });
 };
+
+rename();
