63e77453568945db7dc45870c9ed3f48aa67dc0f
30Owls <oforolega@gmail.com>
2022-06-01 19:36:06 +0300

    fix(fs): list.js - done

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,13 @@
+import fs from 'fs';
+
 export const list = async () => {
-    // Write your code here 
+    const dirName = './src/fs/files'
+    fs.readdir(dirName, (err, files) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+        console.log(files);
+    });
 };
+
+list();
