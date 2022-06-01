0acbd6531d9a316dad93baf3253397cc8f6cf06d
30Owls <oforolega@gmail.com>
2022-06-01 19:52:05 +0300

    fix(fs): read.js - done

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,13 @@
+import fs from 'fs';
+
 export const read = async () => {
-    // Write your code here 
+    let fileToRead = './src/fs/files/fileToRead.txt'
+    fs.readFile(fileToRead, 'utf-8', (err, data) => {
+        if (err) {
+            throw new Error('FS operation failed');
+        }
+        console.log(data);
+    });
 };
+
+read();
