e486d6b8fc5068a8e1107e448794f3441482bc39
30Owls <oforolega@gmail.com>
2022-06-02 14:41:16 +0300

    fix(cli): args.js - done

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,11 @@
 export const parseArgs = () => {
-    console.log(process.argv)
+    let argsString = '';
+
+    for(let i = 2; i < ((process.argv).length); i=i+2){
+        argsString+=`${(process.argv[i]).slice(2)} is ${process.argv[i+1]}, `;
+    }
+
+    console.log(argsString.slice(0, argsString.length - 2));
 };
 
 parseArgs();
