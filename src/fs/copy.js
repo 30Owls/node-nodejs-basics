d60f4f0682a17e1b6cd81ea7f06b5c5bce500e69
30Owls <oforolega@gmail.com>
2022-06-02 12:35:42 +0300

    fix: add path module to copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,13 +1,21 @@
 import fs from 'fs';
+import { fileURLToPath } from 'url'
+import path from 'path'
+import { dirname } from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export const copy = async () => {
-    let srcDir = './src/fs/files';
-    let targetDir = './src/fs/files_copy';
+    let srcDir = path.join(__dirname, 'files');
+    let targetDir = path.join(__dirname, 'files_copy');
+    
     fs.access(srcDir, (err) => {
+        //if there is no srcDir - throw err
         if(err){
             throw new Error('FS operation failed');
         }
         fs.access(targetDir, (err) => {
+            //if there is no targetDir -> err -> create targetDir
             if(err){
                 fs.cp(srcDir, targetDir, { recursive: true }, (err) => {
                     if (err) {
@@ -15,6 +23,7 @@ export const copy = async () => {
                     }
                 });
             }
+            // fs.access sees "files_copy" -> throw err
             else {
                 throw new Error('FS operation failed');
             }
