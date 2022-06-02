2dc1ebeb6f4e6a10e97927c873615c0df28517f1
30Owls <oforolega@gmail.com>
2022-06-02 12:42:53 +0300

    fix: add path module to list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,7 +1,13 @@
 import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export const list = async () => {
-    const dirName = './src/fs/files'
+    const dirName = path.join(__dirname, 'files');
+    
     fs.readdir(dirName, (err, files) => {
         if (err) {
             throw new Error('FS operation failed');
