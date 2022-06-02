ed73d7302051992a68eeb52cfc9be9f882337b1e
30Owls <oforolega@gmail.com>
2022-06-02 11:42:06 +0300

    fix: add path module to create

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,7 +1,12 @@
 import fs from 'fs';
+import { fileURLToPath } from 'url'
+import { dirname } from 'path'
+import path from 'path'
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 export const create = async () => {
-    fs.writeFile('./src/fs/files/fresh.txt', 'I am fresh and young',{flag: "wx"}, function (err) {
+    fs.writeFile(path.join(__dirname, 'files', 'fresh.txt'), 'I am fresh and young',{flag: "wx"}, function (err) {
         if (err) throw new Error('FS operation failed');
     });
 };
