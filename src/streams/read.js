74e775a8d122b546833894941f0e87079ddda4c8
30Owls <oforolega@gmail.com>
2022-06-03 22:04:49 +0300

    feat: implement fileToRead to stdout

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,3 +1,14 @@
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+import path from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 export const read = async () => {
-    // Write your code here 
+    let fileToRead = path.join(__dirname, 'files', 'fileToRead.txt');
+    const stream = fs.createReadStream(fileToRead, 'utf-8');
+    stream.on('data', chunk => process.stdout.write(chunk + '\n'));
 };
+
+read();
