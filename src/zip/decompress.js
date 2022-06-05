54feafdb911bd055695b4b51f3ee6839b0499685
30Owls <oforolega@gmail.com>
2022-06-05 12:58:56 +0300

    fix: no file err in decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -10,18 +10,25 @@ const __dirname = dirname(__filename);
 export const decompress = async () => {
     const fileToDeCompress = path.join(__dirname, 'files', 'archive.gz');
     const decompressedFile = path.join(__dirname, 'files','fileToCompress.txt');
-
-    const imputSream = fs.createReadStream(fileToDeCompress);
-    const outputStream = fs.createWriteStream(decompressedFile);
-
-    pipeline(
-        imputSream,
-        zlib.createUnzip(),
-        outputStream,
-        err => {
-            console.log(err)
-        }
-    );
+    const imputSream = fs.createReadStream(fileToDeCompress,{flag: "r"});
+    
+    //check if archive.gz exists bc fileToCompress.txt become empty if it doesnt
+    function checkFileExists(file) {
+        return fs.promises.access(file, fs.constants.F_OK)
+            .then(() => {
+                const outputStream = fs.createWriteStream(decompressedFile);
+                pipeline(
+                    imputSream,
+                    zlib.createUnzip(),
+                    outputStream,
+                    err => {
+                        console.log(err)
+                    }
+            )})
+            .catch((err) => console.log(err))
+    }
+    
+    checkFileExists(fileToDeCompress)
 };
 
 decompress();
